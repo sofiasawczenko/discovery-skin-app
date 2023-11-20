0703062956802dbe7b2ad82cbc161b7f9a8655f3
Leandroyyy <leandro.cavallari123@gmail.com>
2023-11-20 13:03:18 -0300

    feat: created menu as default layout

---
+++ src/layout/default-layout/index.jsx
@@ -0,0 +1,12 @@
+import { Menu } from "../../components/menu";
+import { MenuContainer } from "./styles";
+
+
+export function DefaultLayout() {
+
+  return (
+    <MenuContainer>
+      <Menu/>
+    </MenuContainer>
+  )
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,6 @@
 import { ChartBar, FileSearch, FirstAid, Share } from "phosphor-react";
 import MainScreen from "../../assets/cellphone/main-screen-cellphone.svg";
 import ThreeCellphones from "../../assets/cellphone/three-cellphones.svg";
-import { Menu } from "../../components/menu";
 import {
   AppInformationContainer,
   AppInformationTitleContainer,
@@ -14,46 +13,43 @@ import {
 
 export function Home() {
   return (
-    <>
-      <Menu />
-      <HomeContainer>
-        <ApresentationContainer>
-          <ApresentationTitleContainer>
-            <h1>NÃO ESPERE SENTIR NA PELE</h1>
-            <p>
-              O Discovery Skin está com você no monitoramento das manchas
-              desconhecidas
-            </p>
-          </ApresentationTitleContainer>
-          <MainScreenPhoneImg src={MainScreen} alt="two cellphones image" />
-        </ApresentationContainer>
+    <HomeContainer>
+      <ApresentationContainer>
+        <ApresentationTitleContainer>
+          <h1>NÃO ESPERE SENTIR NA PELE</h1>
+          <p>
+            O Discovery Skin está com você no monitoramento das manchas
+            desconhecidas
+          </p>
+        </ApresentationTitleContainer>
+        <MainScreenPhoneImg src={MainScreen} alt="two cellphones image" />
+      </ApresentationContainer>
 
-        <AppInformationContainer>
-          <ThreeCellphonesImg src={ThreeCellphones} alt="three cellphones" />
+      <AppInformationContainer>
+        <ThreeCellphonesImg src={ThreeCellphones} alt="three cellphones" />
 
-          <AppInformationTitleContainer>
-            <h1>Um aplicativo completo</h1>
+        <AppInformationTitleContainer>
+          <h1>Um aplicativo completo</h1>
 
-            <ul>
-              <li>
-                <FirstAid color="#FF7A00" size={32} /> Agende as suas consultas
-                em clínicas e hospitais
-              </li>
-              <li>
-                <FileSearch color="#FF7A00" size={32} /> Registre as suas
-                manchas para observar padrões
-              </li>
-              <li>
-                <ChartBar color="#FF7A00" size={32} /> Receba previsões das
-                manchas através de IA
-              </li>
-              <li>
-                <Share color="#FF7A00" size={32} /> Envio facilitado aos médicos
-              </li>
-            </ul>
-          </AppInformationTitleContainer>
-        </AppInformationContainer>
-      </HomeContainer>
-    </>
+          <ul>
+            <li>
+              <FirstAid color="#FF7A00" size={32} /> Agende as suas consultas em
+              clínicas e hospitais
+            </li>
+            <li>
+              <FileSearch color="#FF7A00" size={32} /> Registre as suas manchas
+              para observar padrões
+            </li>
+            <li>
+              <ChartBar color="#FF7A00" size={32} /> Receba previsões das
+              manchas através de IA
+            </li>
+            <li>
+              <Share color="#FF7A00" size={32} /> Envio facilitado aos médicos
+            </li>
+          </ul>
+        </AppInformationTitleContainer>
+      </AppInformationContainer>
+    </HomeContainer>
   );
 }
